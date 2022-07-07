d74d7abfc83eeb86042a29c72e348cab938e7e59
Jlvdwall <joachim.vandewalle@ugent.be>
2022-07-08 01:32:14 +0200

    added setup script

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name="micmec",
+    version="1.0",
+    description="The micromechanical model.",
+    author="Joachim Vandewalle",
+    packages=["micmec"],
+    zip_safe=False)
